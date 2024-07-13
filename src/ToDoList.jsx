af48892302be0153b1305cbde130fc29a1bf392f
Shreekant Chavan <shreekantchavanfycs@gmail.com>
2024-07-14 00:40:37 +0530

    Key for List item's, Deleting, Updating

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,18 +1,51 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ToDo() {
 
-    let [todos, setTodos] = useState(["Sample State"]);
+    let [todos, setTodos] = useState([{task : "Sample-Task", id : uuidv4()}]);
 
     let [newTodos, setNewTodos] = useState("");
 
     let addNewTask = () => {
-        setTodos([...todos, newTodos]);
+        setTodos( (prevTodos) => {
+            return [...prevTodos, {task : newTodos, id : uuidv4()}]
+        });
         setNewTodos("");
     }
 
     let updateTodoValue = (event) => {
         setNewTodos (event.target.value);
+    }
+
+    let deleteTodo = (id) => {
+        setTodos ( (prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+    }
+
+    let UpperCaseAll = () => {
+        setTodos( (prevTodos) => 
+            prevTodos.map( (todo) => {
+                return {
+                    ...todo,
+                    task: todo.task.toUpperCase(),
+                };
+            })
+        );
+    };
+
+    let UpperCaseOne = (id) => {
+        setTodos( (prevTodos) => 
+            prevTodos.map( (todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        task: todo.task.toUpperCase(),
+                    };
+                } else {
+                    return todo;
+                }
+            })
+        );
     }
 
     return (
@@ -26,10 +59,17 @@ export default function ToDo() {
             <ul>
                 {
                     todos.map( (todo) => (
-                        <li>{todo}</li>
+                        <li key={todo.id}>
+                            <span>{todo.task}</span>
+                            &nbsp; &nbsp; &nbsp;
+                            <button onClick={ () => deleteTodo(todo.id)}>Delete</button> &nbsp;
+                            <button onClick={ () => UpperCaseOne(todo.id)}>UpperCase One</button>
+                        </li>
                     ))
                 }
             </ul>
+            <br /><br />
+            <button onClick={UpperCaseAll}>Upper Case All</button>
         </div>
     );
 }
